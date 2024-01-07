200df899f725a4a8d2a7dde4cb17865e5c9257d8
itsmejayd <131668851+itsmejayd@users.noreply.github.com>
2024-01-07 16:02:02 -0500

    refactor csv name and lcoation in html

--- visualize/app.py
+++ visualize/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Load your CSV data using pandas
-    df = pd.read_csv('deliverables/stock_screener_grouped_for_AMKR,FORM,RMBS,LSCC,MTSI,ALGM,WOLF,QRVO,IPGP,POWI,SYNA.csv')
+    df = pd.read_csv('visualize\stock_screener_grouped_for_AAPL, MSFT, GOOG, AMZN.csv')
 
     # Convert the DataFrame to JSON for easier manipulation in JavaScript
     data_json = df.to_json(orient='records')
